ffcbe83d393ef4f62fb382525d08ad27673ccb3e
지민 <wlals425315@gmail.com>
2022-06-16 16:39:54 +0900

    Update - button action query

--- database/app/src/main/java/com/example/database/MainActivity.java
+++ database/app/src/main/java/com/example/database/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.hardware.input.InputManager;
@@ -38,10 +40,10 @@ public class MainActivity extends AppCompatActivity {
 
 
         myDBHelper myDBHelper = new myDBHelper(this);
-        //db = myDBHelper.getWritableDatabase();
+//        db = myDBHelper.getWritableDatabase();
 //        db.close();
 
-        btnInsert.setOnClickListener(new View.OnClickListener() {
+ /*       btnInsert.setOnClickListener(new View.OnClickListener() { // Insert with Query
             @Override
             public void onClick(View view) {
                 db= myDBHelper.getWritableDatabase();
@@ -58,7 +60,70 @@ public class MainActivity extends AppCompatActivity {
                 im.hideSoftInputFromWindow(edtName.getWindowToken(),0);
                 edtName.requestFocus();
             }
+        });*/
+
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                db = myDBHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("gName",edtName.getText().toString());
+                values.put("gNumber",edtNumber.getText().toString());
+                db.insert("groupTBL",null,values);
+                db.close();
+                Toast.makeText(getApplicationContext(),"입력완료", Toast.LENGTH_SHORT).show();
+                edtName.setFocusable(true);
+                edtNumber.setText("");
+
+
+                InputMethodManager im = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                im.hideSoftInputFromWindow(edtName.getWindowToken(),0);
+                edtName.requestFocus();
+
+            }
         });
+        btnSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                db= myDBHelper.getReadableDatabase();
+                Cursor cursor;
+                cursor = db.rawQuery("select * from groupTBL;",null);
+
+                String strNames = "그룹이름" +"\r\n" +"------------" + "\r\n";
+                String strNumbers = "그룹인원" +"\r\n" +"------------" + "\r\n";
+                while(cursor.moveToNext()){
+                    strNames += cursor.getString(0)+ "\r\n";
+                    strNumbers += cursor.getString(1)+ "\r\n";
+                }
+                edtNameResult.setText(strNames);
+                edtNumberResult.setText(strNumbers);
+
+                cursor.close();
+                db.close();
+            }
+        });
+        btnInit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                db = myDBHelper.getWritableDatabase();
+                myDBHelper.onUpgrade(db,1,2);
+                db.close();
+            }
+        });
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                db = myDBHelper.getWritableDatabase();
+                String sql = "Delete from  groupTBL where gName ='"
+                        + edtName.getText().toString() +"'; ";
+                if(edtName.getText().toString()!="") {
+                    db.execSQL(sql);
+                }
+                db.close();
+                btnSelect.callOnClick();
+            }
+        });
+
     }
 
     public class myDBHelper extends SQLiteOpenHelper{ 
